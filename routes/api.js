e9783d88c1c6caa6d3a06f36d7c7dddc9df7ad2e
Joshen Lim <joshen@supabase.io>
2022-03-18 10:31:25 +0100

    asdasd

--- routes/api.js
+++ routes/api.js
@@ -2,8 +2,8 @@ var express = require("express");
 var router = express.Router();
 
 router.get("/", function (req, res, next) {
-  console.log(req.query);
-  res.send({ ping: "pong" });
+  console.log("Query", req.query.q);
+  res.send({ name: "JandJ" });
 });
 
 module.exports = router;
